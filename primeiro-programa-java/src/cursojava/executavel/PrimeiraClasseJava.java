9e83f10142d8c6b3ed145ea9f4fcd3825c7bbcd8
Marcos Tadeu <pereiratadeu2006@hotmail.com>
2021-07-07 09:11:56 -0300

    Criação do projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,12 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	/*Main é o metodo autoexecutavel do java*/
+	public static void main(String[] args) {
+		
+		/*escreve texto no console*/
+		System.out.println("Meu primeiro programa em java - do curso java web");
+		System.out.println("java e orientado a objeto");
+	}
+}
